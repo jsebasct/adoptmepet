e36734445bb2c994274bfb4461433ededb2abfa9
jcruz <sebastian.cruz@bibamgroup.com>
2020-03-31 11:21:15 -0300

    feat: animal option

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
-  const location = 'Seatle, WA';
+  //   const location = 'Seatle, WA';
+  const ANIMALS = ['dog', 'cat', 'bird', 'horse'];
+  const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('dog');
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           location
-          <input id="location" value="location" placeholder="Location" />
+          <input
+            id="location"
+            value="location"
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
         </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select>
+            id="animal" value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+            <option key="all">ALL</option>
+            {ANIMALS.map(animal => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
