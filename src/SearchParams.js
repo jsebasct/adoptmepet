8dd72f03eaa8561735ac55e8603de1f21cfc406e
jcruz <sebastian.cruz@bibamgroup.com>
2020-04-19 15:04:21 -0300

    feat: using ANIMALS from frontendmasters

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useDropdown from './useDropdown';
+import Pet from './Pet';
+import {ANIMALS} from '@frontendmasters/pet'
+// import {breeds} from '@frontendmasters/pet'
 
 const SearchParams = () => {
-  //   const location = 'Seatle, WA';
-  const ANIMALS = ['dog', 'cat', 'bird', 'horse'];
-  const BREEDS = ['havaneese', 'bulldog'];
-
   const [location, setLocation] = useState('Seattle, WA');
   const [breeds, setBreeds] = useState([]);
 
   // const [animal, setAnimal] = useState('dog');
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
 
+  // const ANIMALS = ['dog', 'cat', 'bird', 'horse'];
+  // const BREEDS = ['havaneese', 'bulldog'];
+
   // const [breed, setBreed] = useState('');
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+
+  // useEffect(() => {
+  //   setBreeds([]);
+  //   setBreed('');
+  //   Pet.breeds('dog').then(console.log, console.error);
+  // });
 
   return (
     <div className="search-params">
@@ -29,7 +37,7 @@ const SearchParams = () => {
         </label>
 
         <AnimalDropdown />
-        <BreedDropdown></BreedDropdown>
+        {/* <BreedDropdown /> */}
 
         <button>Submit</button>
       </form>
